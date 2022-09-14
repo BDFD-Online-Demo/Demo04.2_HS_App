ac47f51a2b4f15b574efba71fcab6ef19fc344ba
BDFD <bdfd2005@gmail.com>
2022-09-14 15:31:42 -0400

    get familiar with react state

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [name, setName] = useState("shaun");
+	const [person, setPerson] = useState({ name: "Mario", age: 40 });
+
+	const clickHandler = () => {
+		setName("chu-li");
+		setPerson({
+			name: "luigi",
+			age: 45,
+		});
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>My name is {name}</Text>
+			<Text>His name is {person.name}</Text>
+			<Text>His age is {person.age}</Text>
+			<View style={styles.butoonContainer}>
+				<Button title="update state" onPress={clickHandler} />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	butoonContainer: {
+		marginTop: 20,
+	},
 });
