c76bd1e638868e246bdd02b00f1fd639b22fda62
BDFD <bdfd2005@gmail.com>
2022-09-15 13:06:39 -0400

    update Flat List and Touchable Item

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	FlatList,
+	TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+	const [people, setPeople] = useState([
+		{ name: "shaun", id: "1" },
+		{ name: "yoshi", id: "2" },
+		{ name: "mario", id: "3" },
+		{ name: "luigi", id: "4" },
+		{ name: "peach", id: "5" },
+		{ name: "toad", id: "6" },
+		{ name: "bowser", id: "7" },
+	]);
+
+	const pressHander = (id) => {
+		console.log(id);
+		setPeople((prevPeople) => {
+			return prevPeople.filter((person) => person.id != id);
+		});
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<FlatList
+				numColumns={2}
+				keyExtractor={(item) => item.id}
+				data={people}
+				renderItem={({ item }) => (
+					<TouchableOpacity onPress={() => pressHander(item.id)}>
+						<Text style={styles.item}>{item.name}</Text>
+					</TouchableOpacity>
+				)}
+			/>
 		</View>
 	);
 }
@@ -13,7 +45,17 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
+		paddingTop: 40,
+		paddingHorizontal: 20,
+		// alignItems: "center",
+		// justifyContent: "center",
+	},
+	item: {
+		marginTop: 24,
+		padding: 30,
+		backgroundColor: "pink",
+		fontSize: 24,
+		marginTop: 24,
+		marginHorizontal: 10,
 	},
 });
