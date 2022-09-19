1491aa0a3d2a7fe4e87da47c23537b1219b43999
BDFD <bdfd2005@gmail.com>
2022-09-19 10:34:29 -0400

    update font style

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Text } from "react-native";
+import * as Font from "expo-font";
 import Home from "./screens/home";
+import { AppLoading } from "expo";
+
+const getFonts = () => {
+	return Font.loadAsync({
+		"nanito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+		"nanito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+	});
+};
 
 export default function App() {
-	return <Home />;
-}
+	const [fontsLoaded, setFontsLoaded] = useState(false);
 
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		backgroundColor: "#fff",
-// 		alignItems: "center",
-// 		justifyContent: "center",
-// 	},
-// });
+	if (fontsLoaded) {
+		return <Home />;
+	} else {
+		return (
+			<AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+		);
+	}
+}
