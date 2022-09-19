dc63a062e7db7a0378a3f3529b91337373c5e36d
BDFD <bdfd2005@gmail.com>
2022-09-19 11:04:06 -0400

    useFont alternative method

--- App.js
+++ App.js
@@ -1,36 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Home from "./screens/home";
-import { StyleSheet } from "react-native";
-import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
-
-const getFonts = () =>
-	Font.loadAsync({
-		"Nunito-Regular": require("./assets/fonts/Nunito-Regular.ttf"),
-		"Nunito-Bold": require("./assets/fonts/Nunito-Bold.ttf"),
-	});
+import { useFonts } from "expo-font";
 
 export default function App() {
-	const [fontsLoaded, setFontsLoaded] = useState(false);
+	let [fontsLoaded] = useFonts({
+		"nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+		"nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+	});
 
-	if (fontsLoaded) {
-		return <Home />;
-	} else {
-		return (
-			<AppLoading
-				startAsync={getFonts}
-				onFinish={() => setFontsLoaded(true)}
-				onError={() => console.log("error")}
-			/>
-		);
+	if (!fontsLoaded) {
+		return <AppLoading />;
 	}
-}
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+	return <Home />;
+}
--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,20 @@
+/*
+ * @Date         : 2022-09-16 16:02:15
+ * @Author       : BDFD,bdfd2005@gmail.com
+ * @Github       : https://github.com/bdfd
+ * @LastEditTime : 2022-09-19 11:02:55
+ * @LastEditors  : BDFD
+ * @Description  :
+ * @FilePath     : \screens\home.js
+ * Copyright (c) 2022 by BDFD, All Rights Reserved.
+ */
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 export default function Home() {
 	return (
 		<View style={styles.container}>
-			<Text>Home Screens</Text>
+			<Text style={styles.titleText}>Home Screens</Text>
 		</View>
 	);
 }
@@ -12,5 +22,9 @@ export default function Home() {
 const styles = StyleSheet.create({
 	container: {
 		padding: 24,
+	},
+	titleText: {
+		fontFamily: "nunito-bold",
+		fontSize: 18,
 	},
 });
